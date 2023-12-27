4ccf6271750bf3047d58c2264e177d0168e75ffa
Ahmad Fajar Kusumajati <fajarku26@gmail.com>
2023-12-27 09:39:13 +0700

    first commit, created login

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'poliklinik';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
